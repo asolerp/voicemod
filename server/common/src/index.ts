0a68f825d59b23a6cd28c6114b4a18bb13112919
Alberto Soler <asoler@topdoctors.es>
2020-09-16 12:28:39 +0200

    Added auth middlewares and new error

--- server/common/src/index.ts
+++ server/common/src/index.ts
@@ -1,6 +1,9 @@
 // ERRORS
 export * from './errors/custom-error'
 export * from './errors/not-found-error'
+export * from './errors/not-authorized-error';
 
 // MIDDLEWARES
-export * from './middlewares/error-handler'
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
