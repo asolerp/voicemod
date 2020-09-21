b208b8d192054a2f21b332271dedbb598cc0f323
Alberto Soler <asoler@topdoctors.es>
2020-09-21 16:43:57 +0200

    Removed unnecessary route

--- server/auth/src/routes/current-user.ts
+++ server/auth/src/routes/current-user.ts
@@ -4,12 +4,6 @@ import { User } from '../models/user'
 
 const router = express.Router()
 
-router.get('/hola', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hola desde el server!'
-  });
-})
-
 router.get('/api/users/currentuser',currentUser,(req: Request, res: Response) => {
   res.status(200).send({ currentUser: req.currentUser || null })
 })
